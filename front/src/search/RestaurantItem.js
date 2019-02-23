c748bb6e848a4dbfd23e6f97b2ba0aba5e786e74
Prudence <prudence@blueanchor.io>
2019-02-23 12:28:30 +0100

    front restaurants item component

--- front/src/search/RestaurantItem.js
+++ front/src/search/RestaurantItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card } from 'reactstrap';
   import './RestaurantItem.scss';
 
 const RestaurantItem = (props) => {
@@ -11,12 +10,28 @@ const RestaurantItem = (props) => {
     {filteredRestaurants.map(restaurant => (
       <div key={restaurant.id}>
       <Card className="card">
-        <div className='bloc_img'>
-          <img className="cardimg" src={restaurant.imageUrl} alt="Card cap" />
+        <div className='bloc_img' style={{backgroundImage:`url(${restaurant.imageUrl})`}}>
         </div>
-        <div className="bloc_text">
-          <h3>{restaurant.name}</h3>
-          <p>{restaurant.annotation}</p>
+        <div className="bloc_infos">
+          <div className="info_item">
+            <h3>{restaurant.name}</h3>
+            <div>Rating</div>          
+          </div>
+          <div className="bloc_text">
+            <p>{restaurant.annotation}</p>
+          </div>
+          <div className="info_item">
+            <div className="info_item">
+              <div>
+                <img className="primary_category" src={restaurant.primaryCategory} alt={restaurant.primaryCategory}/>
+              </div>
+              <span className="secondary_category">{restaurant.secondaryCategory}</span>
+            </div>
+            <div className="info_item">
+              <button>Favoris</button>
+              <button>Voir plus</button>
+            </div>
+          </div>
         </div>
       </Card>
       </div>))}
